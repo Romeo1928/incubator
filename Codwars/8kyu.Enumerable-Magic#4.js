c37d8ffab023c0d5d6d94851f4adfb99838e4661
romeo <rjameson1928@gmail.com>
2022-12-05 15:10:48 +0300

    codewars

---
+++ Codwars/8kyu.Enumerable-Magic#4.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/545993ee52756d98ca0010e1/train/javascript
+
+none = (arr, fun) => !arr.some(fun);
+//====================================
+
+// function none(arr, fun){
+// 	 for (var i = 0; i < arr.length; i++) {
+// 		  if (fun(arr[i]) == true) {
+// 				return false;
+// 		  }
+// 	 }
+//
+// 	 return true;
+// }
+
