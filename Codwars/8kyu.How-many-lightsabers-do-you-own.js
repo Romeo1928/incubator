fd67d14b26de000f0847dd98277210811cd726bc
romeo <rjameson1928@gmail.com>
2022-12-06 18:47:36 +0300

    codewars

---
+++ Codwars/8kyu.How-many-lightsabers-do-you-own.js
@@ -0,0 +1,10 @@
+function howManyLightsabersDoYouOwn(name) {
+	 if (name == 'Zach'){
+		  return 18
+	 } else {
+		  return 0
+	 }
+}
+//=============================
+
+let howManyLightsabersDoYouOwn = name => name == "Zach" ? 18 : 0;
