03f438a207a9d6c9169d999e2402ea19fa234b89
romeo <rjameson1928@gmail.com>
2022-12-07 12:12:21 +0300

    codewars

---
+++ Codwars/8kyu.Remove-exclamation-marks.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+function removeExclamationMarks(s) {
+	 return s.replace(/!/g, '');
+}
+//=====================================
+
+const remove = (s) => s.replace(/!/g, '');
