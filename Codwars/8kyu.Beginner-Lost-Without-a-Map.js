ce0b5735174d2029e10a582681239ce92f08d415
romeo <rjameson1928@gmail.com>
2022-12-08 12:10:19 +0300

    codewars

---
+++ Codwars/8kyu.Beginner-Lost-Without-a-Map.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+function maps(x){
+	 let arr = [];
+	 for (let i = 0; i < x.length; i++){
+		  arr.push(x[i] * 2);
+	 }
+	 return arr;
+}
