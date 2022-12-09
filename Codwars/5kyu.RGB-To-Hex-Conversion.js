66567ac6d64dcd4c8303f82981d46d1929bdee4c
romeo <rjameson1928@gmail.com>
2022-12-09 21:21:57 +0300

    codewars

---
+++ Codwars/5kyu.RGB-To-Hex-Conversion.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/513e08acc600c94f01000001/train/javascript
+
+function rgb(r, g, b){
+	return toHex(r) + toHex(g) + toHex(b);
+}
+// ==========================================
+//
+// function toHex(color) {
+// 	if(color < 0)
+// 		return "00";
+// 	if(color > 255)
+// 		return "FF";
+// 	return ( "0" + (Number(color).toString(16)) )
+// 		.slice(-2)
+// 		.toUpperCase()
+// }
+// // ======================================================
+// function rgb(r, g, b){
+// 	return toHex(r)+toHex(g)+toHex(b);
+// }
+//
+// function toHex(d) {
+// 	if(d < 0 ) {return "00";}
+// 	if(d > 255 ) {return "FF";}
+// 	return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase();
