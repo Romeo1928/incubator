be32720bac871ae2099d6022e7f4c630bb6a2e5e
romeo <rjameson1928@gmail.com>
2022-12-09 21:22:05 +0300

    codewars

---
+++ Codwars/6kyu.Doors-in-the-school.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57c15d314677bb2bd4000017/train/javascript
+
+function doors(n){
+	return parseInt(Math.sqrt(n));
+}
+// ====================================
+doors=(n)=>Math.floor(Math.sqrt(n));
