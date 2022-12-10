35947df038e6e0f9968f6d4d54e9f2c2eb039c83
romeo <rjameson1928@gmail.com>
2022-12-10 12:02:54 +0300

    codewars

---
+++ Codwars/7kyu.Isograms.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+
+function isIsogram(str) {
+	return new Set(str.toLowerCase()).size == str.length
+}
+
+//========================================================
+function isIsogram(str) {
+	var i, j;
+	str = str.toLowerCase();
+	for (i = 0; i < str.length; ++i)
+		for (j = i + 1; j < str.length; ++j)
+			if (str[i] === str[j])
+				return false;
+	return true;
+}
