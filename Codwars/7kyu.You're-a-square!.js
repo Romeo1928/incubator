969945e51599ab7112766acd5341efa664fc7f74
romeo <rjameson1928@gmail.com>
2022-12-10 12:03:45 +0300

    codewars

---
+++ Codwars/7kyu.You're-a-square!.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+
+var isSquare = function (n) {
+	if (n < 0) {
+		return false;
+	} else if (Number.isInteger(Math.sqrt(n))) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//===========================================================
+
+function isSquare(n) {
+	return Math.sqrt(n) % 1 === 0;
+}
