78471b455501c1fad2ac4115edbc8484d0945cc4
romeo <rjameson1928@gmail.com>
2023-01-02 14:47:56 +0300

    fcc - JS

---
+++ freeCodeCamp/JavaScript Algorithms and Data Structures/Basic JavaScript/99.Count-Backwards-With-a-For-Loop.js
@@ -0,0 +1,13 @@
+// Setup
+const myArray = [];
+for (let i = 9; i >= 1; i -= 2)
+	myArray.push(i);
+// Only change code below this line
+
+// ====================================
+
+const myArray = [];
+for (let i = 9; i >= 1; i --)
+	if (i % 2) {
+		myArray.push(i);
+	};
