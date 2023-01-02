5a8227acb37826f3b12befda72a80d6045c0a9be
romeo <rjameson1928@gmail.com>
2023-01-02 14:47:35 +0300

    codewars

--- Codwars/8kyu.Beginner-Reduce-but-Grow.js
+++ Codwars/8kyu.Beginner-Reduce-but-Grow.js
@@ -1,10 +1,18 @@
 // https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
 
 
-function grow(x){
-	 let sum = 1;
-	 for (let i = 0; i< x.length; i++){
-		  sum = sum * x[i];
-	 }
-	 return sum
+function grow(x) {
+	let sum = 1;
+	for (let i = 0; i < x.length; i++) {
+		sum = sum * x[i];
+	}
+	return sum
 }
+
+// =========================================================
+
+const grow = x => x.reduce((a, b) => a * b);
+
+// =========================================================
+
+const grow = x => eval(x.join("*"))
